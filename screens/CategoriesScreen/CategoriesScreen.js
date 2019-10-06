b2441163ba87be4c7cf1008c4dd02e332546d9a1
Alan2304 <sanchezalan95@gmail.com>
2019-10-06 10:54:05 -0500

    Add basic concepts of navigation

--- screens/CategoriesScreen/CategoriesScreen.js
+++ screens/CategoriesScreen/CategoriesScreen.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { View, Text, FlatList,Button } from 'react-native';
+import { View, Text, FlatList, Button, TouchableOpacity, Platform } from 'react-native';
 import {CATEGORIES} from '../../data/dummy-data';
 import styles from './styles';
-
-const renderGridItem = (itemData) => {
-    return (
-        <View style={styles.gridItem}>
-            <Text>{itemData.item.title}</Text>
-        </View>
-    )
-}
+import Colors from '../../constants/Colors';
 
 const CategoriesScreen = props => {
+    
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                 props.navigation.navigate({
+                    routeName: 'CategoryMeals', 
+                    params: {
+                        categoryId: itemData.item.id
+                    } 
+                })
+            }}>
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+    
     return (
         <FlatList 
             keyExtractor={(item, index) => item.id}
@@ -19,6 +30,14 @@ const CategoriesScreen = props => {
             renderItem={renderGridItem} 
             numColumns={2} />
     )
+}
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : 'white',
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 }
 
 export default CategoriesScreen;
