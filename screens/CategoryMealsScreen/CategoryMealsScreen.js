99be531329ebd93ac83f73a3bb1888d19f7fad05
Alan2304 <sanchezalan95@gmail.com>
2019-10-06 10:54:46 -0500

    Add parameter received by navigation

--- screens/CategoryMealsScreen/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen/CategoryMealsScreen.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { View, Text, Button } from 'react-native';
 import styles from './styles';
 
+import { CATEGORIES } from '../../data/dummy-data';
+
 const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+    const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go to Meal Detail" onPress={() => {
                 props.navigation.navigate('MealDetail')
             }} />
