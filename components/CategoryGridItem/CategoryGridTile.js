8fef6e08d997ee55834004e6955162471debe9d2
Alan2304 <sanchezalan95@gmail.com>
2019-10-06 16:39:59 -0500

    Add component for the grid item of the category

---
+++ components/CategoryGridItem/CategoryGridTile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, TouchableNativeFeedback, Platform } from 'react-native';
+import styles from './styles';
+
+
+const CategoryGridTile = props => {
+    
+    let TouchableCmp = TouchableOpacity;
+    if (Platform.OS === 'android' && Platform.Version >= 21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
+    
+    return (
+        <View style={styles.gridItem}>
+        <TouchableCmp style={{flex: 1}} onPress={props.onSelect}>
+           <View style={{...styles.container, ...{backgroundColor: props.color}}}>
+               <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
+           </View>
+       </TouchableCmp>
+       </View>
+    )
+}
+
+export default CategoryGridTile;
