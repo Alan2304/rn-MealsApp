33a41dcf17f52dee04b231d9d6ad015e7ee8147e
Alan2304 <sanchezalan95@gmail.com>
2019-10-12 12:03:13 -0500

    Add component for each meal item

---
+++ components/MealItem/style.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    mealRow: {
+        flexDirection: 'row',
+        padding: 10
+    },
+    mealItem: {
+        height: 200,
+        width: '100%',
+        backgroundColor: '#f5f5f5',
+        borderRadius: 10,
+        overflow: 'hidden'
+    },
+    mealHeader: {
+        height: '85%',
+    },
+    mealDetail: {
+        paddingHorizontal: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '15%'
+    },
+    bgImage: {
+        width: '100%',
+        height: '100%',
+        justifyContent: 'flex-end'
+    },
+    titleContainer: {
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        paddingVertical: 5,
+        paddingHorizontal: 12,
+    },  
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        color: 'white',
+        textAlign: 'center'
+    }
+});
+
+export default styles;
