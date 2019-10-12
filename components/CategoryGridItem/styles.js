a4316b1c093540a05c8b576ed45513de287ebada
Alan2304 <sanchezalan95@gmail.com>
2019-10-12 16:36:20 -0500

    Quick fix problem with the shadow and elevation

--- components/CategoryGridItem/styles.js
+++ components/CategoryGridItem/styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 const styles = StyleSheet.create({
     gridItem: {
@@ -6,7 +6,8 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden' : 'visible',
+        elevation: 5,
     },
     container: {
         flex: 1,
@@ -15,7 +16,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: {width: 0, height: 2},
         shadowRadius: 10,
-        elevation: 5,
         padding: 15,
         justifyContent: 'flex-end',
         alignItems: 'flex-end'
