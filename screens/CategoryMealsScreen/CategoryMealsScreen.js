77df1f1dbcb6f5c32ec3653931977e72cdeaa469
Alan2304 <sanchezalan95@gmail.com>
2019-10-12 12:03:56 -0500

    Add the component MealItem for each item of the Flat List

--- screens/CategoryMealsScreen/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen/CategoryMealsScreen.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import styles from './styles';
 
-import { CATEGORIES } from '../../data/dummy-data';
+import { CATEGORIES, MEALS } from '../../data/dummy-data';
+
+import MealItem from '../../components/MealItem/MealItem';
 
 const CategoryMealsScreen = props => {
     const catId = props.navigation.getParam('categoryId');
     const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
+    const displayedMeals =  MEALS.filter(
+        meal => meal.categoryIds.indexOf(catId) >= 0
+    );
+
+    const renderMealItem = (itemData) => {
+        return (
+            <MealItem 
+                title={itemData.item.title}
+                image={itemData.item.imageUrl}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability}
+                onSelectMeal={() => {}}  />
+        )
+    }
+
     return (
         <View style={styles.screen}>
-            <Text>The Category Meals Screen</Text>
-            <Text>{selectedCategory.title}</Text>
-            <Button title="Go to Meal Detail" onPress={() => {
-                props.navigation.navigate('MealDetail')
-            }} />
+            <FlatList 
+                data={displayedMeals} 
+                keyExtractor={(item, index) => item.id}
+                renderItem={renderMealItem}
+                style={{width: '100%'}}/>
         </View>
     )
 }
