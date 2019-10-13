e3fa923804f42de6c22dbff828cb5ec04c242527
Alan2304 <sanchezalan95@gmail.com>
2019-10-13 13:01:39 -0500

    Add filters components

--- screens/FiltersScreen/FiltersScreen.js
+++ screens/FiltersScreen/FiltersScreen.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Switch, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import styles from './styles';
+import Colors from '../../constants/Colors';
 
 import HeaderButton from '../../components/HeaderButton/HeaderButton';
 
+const FilterSwitch = props => {
+    return (
+        <View style={styles.filterContainer}>
+            <Text>{props.label}</Text>
+            <Switch 
+                value={props.state} 
+                trackColor={{true: Colors.primaryColor}}
+                thumbColor={ Platform.OS ==='android' ? Colors.primaryColor : ''}
+                onValueChange={props.onChange} />
+        </View>
+    )
+}
+
 const FiltersScreen = props => {
+
+    const [isGlutenFree, setIsGlutenFree] = useState(false);
+    const [isLactoseFree, setIsLactoseFree] = useState(false);
+    const [isVegan, setIsVegan] = useState(false);
+    const [isVegetarian, setIsVegetarian] = useState(false);
+
     return (
         <View style={styles.screen}>
-            <Text>The Filters Screen</Text>
+            <Text style={styles.title}>Available Filters / Restrictions</Text>
+            <FilterSwitch 
+                label='Gluten Free' 
+                state={isGlutenFree} 
+                onChange={newValue => setIsGlutenFree(newValue)} />
+            <FilterSwitch 
+                label='Lactose Free' 
+                state={isLactoseFree} 
+                onChange={newValue => setIsLactoseFree(newValue)} />
+            <FilterSwitch 
+                label='Vegan' 
+                state={isVegan} 
+                onChange={newValue => setIsVegan(newValue)} />
+            <FilterSwitch 
+                label='Vegetarian' 
+                state={isVegetarian} 
+                onChange={newValue => setIsVegetarian(newValue)} />
+            
         </View>
     )
 }
