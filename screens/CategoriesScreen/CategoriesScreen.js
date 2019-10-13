09fb90df43874c096c6c84f3cd5cee1ff8f59464
Alan2304 <sanchezalan95@gmail.com>
2019-10-13 11:50:41 -0500

    Add style and functionality for the drawer navigation

--- screens/CategoriesScreen/CategoriesScreen.js
+++ screens/CategoriesScreen/CategoriesScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { View, Text, FlatList, Button, TouchableOpacity } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import {CATEGORIES} from '../../data/dummy-data';
 import styles from './styles';
 
 import CategoryGridTile from '../../components/CategoryGridItem/CategoryGridTile';
+import HeaderButton from '../../components/HeaderButton/HeaderButton';
 
 const CategoriesScreen = props => {
     
@@ -32,8 +34,20 @@ const CategoriesScreen = props => {
     )
 }
 
-CategoriesScreen.navigationOptions = {
-    headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions = (navData) =>  {
+    return {
+        headerTitle: 'Meal Categories',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Menu' 
+                    iconName='ios-menu' 
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }} />
+            </HeaderButtons>
+        )
+    }
     
 }
 
--- screens/FiltersScreen/FiltersScreen.js
+++ screens/FiltersScreen/FiltersScreen.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
 import styles from './styles';
+
+import HeaderButton from '../../components/HeaderButton/HeaderButton';
 
 const FiltersScreen = props => {
     return (
@@ -8,6 +12,22 @@ const FiltersScreen = props => {
             <Text>The Filters Screen</Text>
         </View>
     )
+}
+
+FiltersScreen.navigationOptions = (navData) => {
+    return {
+        headerTitle: 'Filter Meals',
+        headerLeft: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Menu'
+                    iconName='ios-menu' 
+                    onPress={() => {
+                        navData.navigation.toggleDrawer();
+                    }} />
+            </HeaderButtons>
+        )
+    }
 }
 
 export default FiltersScreen;
